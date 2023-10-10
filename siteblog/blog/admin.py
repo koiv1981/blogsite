f3a7c7ced18882d292353fae8c5f28b19bead020
koiv1981 <koiv1981@gmail.com>
2023-10-10 11:18:46 +0300

    BlogSite #3 (models, migrations, admin, ckeditor, debug_toolbar )

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from .models import *
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title",)
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title",)
+
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "author", "created_at", "views", "category", "get_photo", "views")
+    list_display_links = ("title", "category")
+    readonly_fields = ("views", "created_at", "get_photo")
+    search_fields = ("title",)
+    list_filter = ("category", "tags")
+    save_as = True
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        else:
+            return "Фото отсутствует"
+
+    get_photo.__name__ = "Миниатюра"
+
+
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ("email", "created_at")
+    readonly_fields = ("email", "created_at")
+    list_filter = ("created_at",)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "content", "created_at", "updated_at", "is_published")
+    readonly_fields = ("created_at", "updated_at")
+    list_filter = ("created_at", "is_published")
+    search_fields = ("name", "email", "content")
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Subscriber, SubscribeAdmin)
+admin.site.register(Comment, CommentAdmin)
--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -1,3 +1,97 @@
 from django.db import models
 
-# Create your models here.
+"""
+Caregory 
+=============
+title, slug 
+
+Tag
+=============
+title, slug 
+
+Post
+============= 
+title, slug, author, contetnt, created_at, photo, views, category, tags
+
+Subscribe
+============
+email, created_at
+
+Comment
+============
+post, name, email, content, creared_at, updated_at, is_published
+"""
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название категории')
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название тэга')
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название поста')
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.CharField(max_length=255, verbose_name='Автор поста')
+    content = models.TextField(blank=True, verbose_name='Содержание поста')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Опубликовано")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True, verbose_name='Изображение')
+    views = models.IntegerField(default=0, verbose_name="Кол-во просмотров")
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="posts", verbose_name='Категория')
+    tags = models.ManyToManyField("Tag", blank=True, related_name='posts', verbose_name='Тэг')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+
+class Subscriber(models.Model):
+    email = models.EmailField(max_length=50, verbose_name='Адрес электронной почты', unique=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата подписки")
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Подписчик"
+        verbose_name_plural = "Подписчики"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.PROTECT, related_name='comments', verbose_name="Пост")
+    name = models.CharField(max_length=80, verbose_name="Имя")
+    email = models.EmailField(verbose_name="Адрес электронной почты")
+    content = models.TextField(verbose_name="Содержание комментария")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлен")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликован")
+
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
+    def __str__(self):
+        return f'Комментарий от {self.name} на {self.post}'
